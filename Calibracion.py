a0ecd21a1038a98fcbc880102b6cacb95ae2054e
McRios07 <61805470+McRios07@users.noreply.github.com>
2020-03-16 22:11:01 -0600

    calibracion

--- Calibracion.py
+++ Calibracion.py
@@ -2,7 +2,6 @@ import Planta
 import Negocio
 import RPi.GPIO as GPIO
 
-
 GPIO.setwarnings(False)
 
 negocio = Negocio.negocio()
@@ -15,17 +14,17 @@ else:
 port = 0
 id = columns[0][0]
 
-sensor = Planta.plant(pin,port,id)
+sensor = Planta.plant(int(pin),port,id)
 
 pinRef = negocio.getSetting("SensorReferencia")
 portRef = 1
 idRef = 0
 
-sensorRef = Planta.plant(pinRef,portRef,idRef)
+sensorRef = Planta.plant(int(pinRef),portRef,idRef)
 atmosphericPressure = sensorRef.getPressure()
 
-initialPressure = negocio.getSetting("presionInicial")
-margenRef = negocio.getSetting("Margen")
+initialPressure = float(negocio.getSetting("presionInicial"))
+margenRef = float(negocio.getSetting("Margen"))
 
 pressureInf =initialPressure - (margenRef/100.0)*initialPressure
 pressureSup =initialPressure + (margenRef/100.0)*initialPressure
@@ -33,8 +32,8 @@ pressureSup =initialPressure + (margenRef/100.0)*initialPressure
 
 while (negocio.getCalibrando(id)[0][0] == 1):
     pressure =sensor.getPressure()
-    negocio.updatePressure(sensor.id,pressure)
+    negocio.updatePressure(id,pressure)
     if ((pressure - atmosphericPressure) > pressureInf and (pressure - atmosphericPressure) < pressureSup):
-        negocio.setCalibrado('1',sensor.id)
-    else
-        negocio.setCalibrado('0',sensor.id)
+        negocio.setCalibrado('1',id)
+    else:
+        negocio.setCalibrado('0',id)
